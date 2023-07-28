443a569bab2c0315745cf58346f6fb67502f99d4
Duzzann <d.miklos8@gmail.com>
2023-07-28 13:41:55 +0200

    aaa

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const printForecast = function(arr){
+    let output = '';
+    for(let i = 0; i<arr.length;i++){
+        output += '...'+arr[i]+'° in '+i+' days';
+    }
+    output+='...';
+    console.log(output);
+};
+printForecast([12,5,-5,0,4]);
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct number! 🎉';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 24;
+*/
+document.querySelector('.check').addEventListener('click',function(){
+    console.log(document.querySelector('.guess').value);
+    document.querySelector('.message').textContent = 'Correct number! 🎉';
+})
