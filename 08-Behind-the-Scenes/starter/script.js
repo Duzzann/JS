ce24c30299723736b8ee3acabe2591e1025e85b1
Duzzann <d.miklos8@gmail.com>
2023-10-05 09:53:52 +0200

    caf

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function calcAge(birthYear){
+    const age = 2037- birthYear;
+    console.log(firstName);
+
+    function printAge(){
+        const output = `You are ${age} years old! And were born in ${birthYear}`;
+        console.log(output);
+
+        if(birthYear >= 1940 && birthYear<= 1996){
+            var millenial = true;
+            const str = `Oh and you're a millenial. ${firstName}`;
+            console.log(str);
+        }
+        console.log(millenial);
+    }
+    printAge();
+
+    return age;
+}
+
+const firstName = 'Dusan';
+calcAge(1945);
